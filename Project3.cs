edf8021f519472371bd8779c66aa168ac1a5fc4b
Kadir Deveci <123473584+DKadir13@users.noreply.github.com>
2024-03-20 03:25:39 +0300

    Add files via upload

---
+++ Project3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Project3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbConnection baglan = new
+            OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data
+            Source=C:\uyeler.xlsx; Extended Properties='Excel 12.0 xml
+            ;HDR=YES'");
+            baglan.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter("SELECT * FROM[Sayfa1$]", baglan);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt.DefaultView;
+            baglan.Close();
+        }
+    }
+}
